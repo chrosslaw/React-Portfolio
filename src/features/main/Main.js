04767a4d91feb13f4f81bb447670c8e65f32687b
Chris Lawrence <Chrosslaw@gmail.com>
2023-03-09 15:55:35 -0600

    main section styling

--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -1,7 +1,16 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 const Main = ({ activePage }) => {
   return (
     <main className={`main ${activePage}`}>
-      <div className={activePage}></div>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="lg" fixed>
+          <Box sx={{ bgcolor: "#ffffff", height: "100vh" }} />
+        </Container>
+      </React.Fragment>
     </main>
   );
 };
